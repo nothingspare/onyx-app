d167f03b5fa82eac84c9cbdf7efb61ea78f83e45
Michael Holleran <michael@bravewhale.com>
2015-07-11 20:40:13 -0700

    gulp concat task added

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,13 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 
 gulp.task('concat', function () {
-	console.log('test');
+	return gulp.src([
+			'./public/app/config/*.js',
+			'./public/app/services/*.js',
+			'./public/app/filters/*.js',
+			'./public/app/directives/*.js',
+			'./public/app/controllers/*.js',
+		])
+		.pipe(concat('application.js'))
+		.pipe(gulp.dest('./public/dest/'));
 });
